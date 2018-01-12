e8f09de6e74675356d552cff99276830b4b5f221
SylviaCheung <sylvia01per@163.com>
2018-01-12 19:16:49 +0800

    Initial commit

---
+++ practice12.3.3.py
@@ -0,0 +1,9 @@
+class Settings():
+	"""storage of Aliens to invasion all classes setting """
+	
+	def __init__(self):
+		""" Initial game setting """
+		# screen setting
+		self.screen_width = 12000
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
