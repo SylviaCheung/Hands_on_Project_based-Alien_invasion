ec83f1c37078bc33ce4c6d3cc866911535198227
SylviaCheung <sylvia01per@163.com>
2018-01-12 19:55:47 +0800

    alien update

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """存储《外星人入侵》的设置的类"""
+
+    def __init__(self):
+        """初始游戏化的设置"""
+        # setting screen
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
